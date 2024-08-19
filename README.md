8602db49fce132ffb4a1e828dab2ab2e4c9dec27
Fernii99 <asierfc@gmail.com>
2024-08-19 10:15:30 +0200

    Project start

---
+++ README.md
@@ -0,0 +1,3 @@
+Project 02 of the series of learning and masterizing REACT,
+
+In this project I will just develop a simple Tic Tac Toe game, with states and local storage saving data
---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+const TURNS =  {
+  X: 'x',
+  O: 'o'
+}
+
+const board = Array(9).fill(null);
+
+function App() {
+
+  return (
+    <main className="board">
+      <h1> TIC TAC TOE</h1>
+      <section className="game">
+        {board.map( (square, index) =>{
+          return ( 
+            <div className="cell" key={index}>
+              <span className="cell_content">
+                {index}
+              </span>
+            </div>
+          )
+        } )}
+      </section>
+    </main>
+  )
+}
+
+export default App
